5332b80c4a35fed2d2f7b8805da5fe784e2e967c
Watheq Attrah <novaatrah@gmail.com>
2023-07-02 19:56:55 -0700

    App.js Update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import React,{ useEffect, useState } from "react";
+import './App.css';
+import React, { useEffect, useState } from "react";
 import { Route, Switch } from "react-router-dom";
-import NavBar from "../src/components/NavBar";
-import CarsList from "../src/components/CarsList";
-import CarForm from "../src/components/CarForm";
-
+import NavBar from "../src/conponents/NavBar";
+import CarsList from "../src/conponents/CarsList";
+import CarForm from "../src/conponents/CarForm";
+import ContactUs from './conponents/ContactUs';
 
 function App() {
   const [cars, setCars] = useState([])
-  // Add useEffect hook for  FETCH JSON
-  useEffect(()=>{
-    fetch("http://localhost:4000/cars")
-    .then(response => response.json())
-    .then(car => setCars(car))
-    //console.log(cars)
-  }, [])
-  
-    //Add car cb + state update
-    function handleAddCar(newCar) {
-      setCars([...cars, newCar])
-    }
 
+  useEffect(()=> {
+    fetchCars("http://localhost:4000/cars");
+  },[])
+  const fetchCars = async () => {
+    try {
+      const response = await fetch(`http://localhost:4000/cars`);
+      const data = await response.json();
+      setCars(data)
+      }catch (error) {
+        console.log(`Error fetching cars: `, error);
+      }
+    };
+//Add Car Function Update State
+  function handleAddCar(newCar) {
+    setCars([...cars, newCar])
+  }
+//App.js return value
   return (
     <div>
       <NavBar />
@@ -30,9 +36,12 @@ function App() {
         <Route exact path="/">
           <CarsList cars={cars} setCars={setCars} />
         </Route>
+        <Route exact path="/ContactUs">
+          <ContactUs />
+        </Route>
+
       </Switch>
     </div>
   )
-}
-
+};
 export default App;
