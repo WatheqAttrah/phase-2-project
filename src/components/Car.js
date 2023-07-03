6bb8eab4f5c616d63898aa301fe190a48783b192
Watheq Attrah <novaatrah@gmail.com>
2023-07-02 19:45:03 -0700

    confirm car.js functions

--- src/components/Car.js
+++ src/components/Car.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 function Car({ cars, onDeleteCar }) {
-  // fetch DELETE and update state thru onDeleteCar prop
+// fetch DELETE and update state thru onDeleteCar prop
   function handleDeleteClick(){
     fetch(`http://localhost:4000/cars/${cars.id}`,{
       method: "DELETE",
@@ -9,7 +9,6 @@ function Car({ cars, onDeleteCar }) {
       .then(response => response.json())
       .then(()=>onDeleteCar(cars))
   };
-
 return (
     <li> 
         <p>Car Make: <b>{cars.make}</b></p>
@@ -17,11 +16,12 @@ return (
         <p>Car Year: <b>{cars.year}</b></p>
         <p>Car Trim: <b>{cars.trim}</b></p>
         <p>Car id: <b>{cars.id}</b></p>
-        <br></br>
-        <button onClick={handleDeleteClick}>Remove</button>    
+        <br></br>    
+        <button onClick={handleDeleteClick}>Delete</button>
         <hr></hr>
     </li>
   )
 }
+
 
 export default Car;
